b4f5a0e9c7b53fcdae46558d5dae0f9a259de103
AwesomestAlt <74363858+AwesomestAlt@users.noreply.github.com>
2021-02-22 15:45:51 -0500

    try writing the problem for photoshoot
    http://www.usaco.org/index.php?page=viewproblem2&cpid=988 if this actually works I'm going to be very surprised

--- src/main/java/io/github/awesomestalt/competitivecoding/ProgramLoader.java
+++ src/main/java/io/github/awesomestalt/competitivecoding/ProgramLoader.java
@@ -1,7 +1,31 @@
 package io.github.awesomestcode.competitivecoding;
 
+import java.util.StringTokenizer;
+
 public class ProgramLoader {
   public static void main(String[] args) {
     System.out.println("Welcome.");
+    photoShoot();
+  }
+  public static void photoShoot() {
+    int n = 5; //replace with scanner in actual run
+    String secondLine = "5 4 7 6"; //same here
+    int[] b = new int[n-1];
+    int[] a = new int[n];
+    StringTokenizer tokenizer = new StringTokenizer(secondLine);
+    int onB = 0;
+    while(tokenizer.hasMoreTokens()) {
+      b[onB] = Integer.parseInt(tokenizer.nextToken());
+      onB++;
+    }
+    for(int i = 1; i <= n; i++) {
+      a[0] = i;
+      for(int j = 1; j < a.length; j++) {
+        a[j] = b[j + 1] - a[j-1];
+      }
+    }
+    for(int element : a) {
+      System.out.print(element + " ");
+    }
   }
 }
